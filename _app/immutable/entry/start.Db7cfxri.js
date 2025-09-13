23c6a812f7321344925911f2cfb1a78aec94a973
mrsauravsahu <mrsauravsahu@outlook.com>
2025-09-13 18:34:28 +0530

    chore: add new landing page for payobills lite

---
+++ _app/immutable/entry/start.Db7cfxri.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/BTjG3GEW.js";export{o as load_css,r as start};
--- _app/immutable/nodes/2.CohymsGw.js
+++
@@ -1,2 +0,0 @@
-import"../chunks/hbSGsm1g.js";import{t as o,h as s}from"../chunks/CG7VwJ5W.js";var i=o(`<section class="svelte-vkitot"><p><span class="svelte-vkitot">payobills lite</span> is your go to app to learn about billing and transaction
-    needs, all without sharing your data with third parties.</p></section>`);function p(t){var a=i();s(t,a)}export{p as component};
