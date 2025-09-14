7655b529b7582b7acd5b1ee4fbac6a3864b1e30d
mrsauravsahu <mrsauravsahu@outlook.com>
2025-09-14 14:25:27 +0530

    chore: remove section background from payobills lite landing page

--- _app/immutable/entry/start.BOJdInGL.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/47It6AlZ.js";export{o as load_css,r as start};
---
+++ _app/immutable/nodes/2.Caldk5ar.js
@@ -0,0 +1,12 @@
+import"../chunks/DsnmJJEf.js";import"../chunks/CNYE0NCz.js";import{g as y,i as f,j as w,k as s,l as t,m as d,n as p,o as a,r}from"../chunks/JJwanOJM.js";import"../chunks/DyQmpbbk.js";import{I as h,f as _,a as k,b as S}from"../chunks/C52QL5KQ.js";var B=y(`<section class="svelte-relhb6"><h2 class="svelte-relhb6">Bills be gone.</h2> <p class="one-liner svelte-relhb6"><span class="svelte-relhb6">payobills lite</span> is your go to app to be on top of your billing and transaction needs, all without sharing your data with third
+    y services. BKYOD: You bring and keep your own data with you.</p> <div class="card svelte-relhb6"><!> <h3 class="svelte-relhb6">Bill Management</h3> <p class="svelte-relhb6">Track all your bills together in a single place. No need to do it across different apps.</p></div> <div class="card svelte-relhb6"><!> <h3 class="svelte-relhb6">Reminders</h3> <p class="svelte-relhb6">Get notified about when to pay your bills, so you never miss a due date.</p></div> <div class="card svelte-relhb6"><!> <h3 class="svelte-relhb6">Detailed Transaction History</h3> <p class="svelte-relhb6">See where your money goes with a clear, categorized view of your monthly
+      spending.</p></div> <button class="svelte-relhb6">Coming Soon</button> <p class="one-liner svelte-relhb6">* Payobills is an open source project maintained by <span class="svelte-relhb6">Sahu, S</span> in his personal time. ❤️ is appreciated</p></section>`);function x(v){const l=(e=[])=>`
+    background: linear-gradient(to right, ${e.join(", ")});
+    border: none;
+    color: white;
+    padding: 1.6rem;
+    margin-bottom: 1rem;
+    border-radius: 2rem;
+    cursor: pointer;
+    transition: background 0.3s ease;
+  `;var o=B(),n=s(t(o),4),b=t(n);{let e=p(()=>l(["#C850A3","#8341D7"]));h(b,{get icon(){return _},scale:.75,get style(){return d(e)}})}a(4),r(n);var i=s(n,2),u=t(i);{let e=p(()=>l(["#61C1C7","#518EE5"]));h(u,{get icon(){return k},scale:.75,get style(){return d(e)}})}a(4),r(i);var c=s(i,2),g=t(c);{let e=p(()=>l(["#F0B347","#E98739"]));h(g,{get icon(){return S},scale:.75,get style(){return d(e)}})}a(4),r(c);var m=s(c,2);a(2),r(o),f("click",m,()=>{}),w(v,o)}export{x as component};
